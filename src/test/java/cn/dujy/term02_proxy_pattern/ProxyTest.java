abb8919e15d48e8d0daff94fef90f7dc38e9eaaa
dujiayuan <dujy1024@outlook.com>
2024-04-18 11:38:10 +0800

    优化

--- src/test/java/cn/dujy/term02_proxy_pattern/ProxyTest.java
+++ src/test/java/cn/dujy/term02_proxy_pattern/ProxyTest.java
@@ -26,21 +26,21 @@ public class ProxyTest {
      */
     @Test
     public void JDKProxy() {
-
-        // 被代理对象（实现类）
+        // 1、被代理对象（实现类）
         IUserDao IUserDaoImpl = new UserDaoImpl();
 
+        // 2、获取：代理类的类加载器、被代理对象的接口、代理程序
         // 类加载器 —— 获取被代理对象的类加载器
         ClassLoader loader = IUserDaoImpl.getClass().getClassLoader();
         // 接口 —— 获取被代理对象的接口
         Class<?>[] interfaces = IUserDaoImpl.getClass().getInterfaces();
         // 代理程序 —— 被代理对象的调用处理程序
         InvocationHandlerImpl invocationHandlerImpl = new InvocationHandlerImpl(IUserDaoImpl);
-        // 创建代理的对象
-        IUserDao userDaoImplProxy = (IUserDao) Proxy.newProxyInstance(loader, interfaces, invocationHandlerImpl);
-        log.info("JDK 代理对象：{}", userDaoImplProxy.getClass());
 
-        // 调用时才指定调用哪个方法
+        // 3、创建代理的对象
+        IUserDao userDaoImplProxy = (IUserDao) Proxy.newProxyInstance(loader, interfaces, invocationHandlerImpl);
+
+        // 4、调用时才指定调用哪个方法
         userDaoImplProxy.save1();
         userDaoImplProxy.save2();
     }
@@ -50,27 +50,23 @@ public class ProxyTest {
      * <p>
      * 关键：<br/>
      * 1、实现 MethodInterceptor 接口<br/>
-     * 2、使用 enhancer.create() 创建代理对象<br/>
+     * 2、使用 Enhancer.create() 创建代理对象
      */
     @Test
     public void CGLIBProxy() {
-        // 创建NodeDao的实例（普通类）
+        // 1、被代理对象（普通类）
         NodeDao nodeDao = new NodeDao();
 
-        // Enhancer用于配置代理对象的生成
-        Enhancer enhancer = new Enhancer();
-        // CGLIB代理类的代理对象的方法拦截器Impl ---》 被代理对象的调用处理程序
+        // 2、获取：代理类的类class、代理程序
+        // 获取【代理对象nodeDao】
+        Class<? extends NodeDao> nodeDaoClass = nodeDao.getClass();
+        // 代理程序 —— 被代理对象的调用处理程序
         MethodInterceptorImpl methodInterceptorImpl = new MethodInterceptorImpl(nodeDao);
-        log.info("CGLIB 代理对象：{}", methodInterceptorImpl.getClass());
-        // 设置【代理对象UserDaoImpl】为超类
-        enhancer.setSuperclass(nodeDao.getClass());
-        // 设置【回调函数】 ---> 即当代理对象的方法被调用时执行的逻辑
-        enhancer.setCallback(methodInterceptorImpl);
-        // 创建代理对象
-        NodeDao nodeDaoProxy = (NodeDao) enhancer.create();
-        log.info("CGLIB 代理对象：{}", nodeDaoProxy.getClass());
 
-        // 调用代理对象的方法，此处调用的是save1方法
+        // 3、创建代理对象
+        NodeDao nodeDaoProxy = (NodeDao) Enhancer.create(nodeDaoClass,methodInterceptorImpl);
+
+        // 4、调用代理对象的方法
         nodeDaoProxy.save1();
         nodeDaoProxy.save2();
     }
