ac1fb2ddfef319c1c7bbd1db94f6a119406bfe41
dujiayuan <dujy1024@outlook.com>
2024-04-18 11:39:06 +0800

    优化

--- src/main/java/cn/dujy/term02_proxy_pattern/CGLIB_proxy/MethodInterceptorImpl.java
+++ src/main/java/cn/dujy/term02_proxy_pattern/CGLIB_proxy/MethodInterceptorImpl.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Method;
 @Slf4j
 public class MethodInterceptorImpl implements MethodInterceptor {
 
-    private Object targetObject;
+    private Object target;
 
     /**
      * 这里的目标类型为Object，则可以接受任意一种参数作为被代理类，实现了动态代理
@@ -19,13 +19,13 @@ public class MethodInterceptorImpl implements MethodInterceptor {
      */
     public MethodInterceptorImpl(Object target) {
         // 设置需要创建子类的类
-        this.targetObject = target;
+        this.target = target;
     }
 
     @Override
     public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
         log.warn("===> 开启事物");
-        Object result = methodProxy.invoke(targetObject, args);
+        Object result = methodProxy.invoke(this.target, args);
         log.warn("===> 关闭事物");
         // 返回代理对象
         return result;
